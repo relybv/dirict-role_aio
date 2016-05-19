d80584da345b7de79f6af7e3a333522e650fd71c
root <root@aio.novalocal>
2016-05-19 14:01:15 +0000

    use contain:q n  enter the commit message for your changes. Lines starting

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,9 +10,22 @@
 class role_aio
 {
   # a role includes one or more profiles and at least a 'base' profile
-  include ::profile_base
-  include ::profile_apache
-  include ::profile_mysql
-  include ::profile_nfs
-  include ::profile_rsyslog
+#  include ::profile_base
+#  include ::profile_apache
+#  include ::profile_mysql
+#  include ::profile_nfs
+#  include ::profile_rsyslog
+
+  contain profile_base
+  contain profile_rsyslog
+  contain profile_mysql
+  contain profile_nfs
+  contain profile_apache
+
+  Class['profile_base' ] ->
+  Class['profile_rsyslog' ] ->
+  Class['profile_mysql' ] ->
+  Class['profile_nfs' ] ->
+  Class['profile_apache' ]
+
 }
