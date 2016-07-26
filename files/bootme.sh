e833a1abff0c263fa935ab1b69fbefc65fcfa594
Paul Gomersbach <p.gomersbach@rely.nl>
2016-07-26 09:55:52 +0000

    add noninteractive force install use gems

--- files/bootme.sh
+++ files/bootme.sh
@@ -3,14 +3,14 @@
 # This script installs puppet 3.x or 4.x and deploy the manifest using puppet apply -e "include role_aio"
 #
 # Usage:
-# Ubuntu / Debian: wget https://raw.githubusercontent.com/pgomersbach/role_aio/master/files/bootme.sh; bash bootme.sh
+# Ubuntu / Debian: wget https://raw.githubusercontent.com/relybv/dirict-role_aio/master/files/bootme.sh; bash bootme.sh
 #
-# Red Hat / CentOS: curl https://raw.githubusercontent.com/pgomersbach/role_aio/master/skeleton/files/bootme.sh -o bootme.sh; bash bootme.sh
+# Red Hat / CentOS: curl https://raw.githubusercontent.com/relybv/dirict-role_aio/master/files/bootme.sh -o bootme.sh; bash bootme.sh
 # Options: add 3 as parameter to install 4.x release
 
 # default major version, comment to install puppet 3.x
 PUPPETMAJORVERSION=4
-
+export DEBIAN_FRONTEND=noninteractive
 ### Code start ###
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root." >&2
@@ -40,7 +40,7 @@ if which apt-get > /dev/null 2>&1; then
     echo "Using yum"
 fi
 
-apt-get install git bundler zlib1g-dev -y || yum install -y git bundler zlib-devel
+apt-get install git bundler zlib1g-dev libaugeas-ruby -y -q || yum install -y git bundler zlib-devel
 
 # get or update repo
 if [ -d /root/role_aio ]; then
@@ -55,18 +55,21 @@ fi
 
 # install puppet if not installed
 if which puppet > /dev/null 2>&1; then
-   echo "Puppet is already installed."
-  else
-    bash /root/role_aio/files/bootstrap.sh $PUPPETMAJOR
+  echo "Puppet is already installed."
+else
+  bash /root/role_aio/files/bootstrap.sh $PUPPETMAJOR
 fi
 
 # prepare bundle
 echo "Installing gems"
-bundle install --path vendor/bundle --without development system_tests
+gem install rake puppetlabs_spec_helper puppet --no-rdoc --no-ri -q
+
 # install dependencies from .fixtures
 echo "Preparing modules"
-bundle exec rake spec_prep
+/usr/local/bin/rake spec_prep
+
 # copy to puppet module location
 cp -a /root/role_aio/spec/fixtures/modules/* $MODULEDIR
+
 echo "Run puppet apply"
 /usr/local/bin/puppet apply -e "include role_aio"
