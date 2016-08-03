656e6f393bb930c73f89f7481813deb3b3b987df
Paul Gomersbach <p.gomersbach@rely.nl>
2016-08-03 09:54:52 +0000

    set staging

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,9 +13,8 @@ class role_aio
   stage{ 'first': }
   stage{ 'second': }
   Stage[first]->Stage[second]
-  # nfs was first
-  class { 'profile_nfs': stage => second, }
-  class { 'profile_nfs::rspec_monitor': stage => second, }
+  class { 'profile_nfs': stage => first, }
+  class { 'profile_nfs::rspec_monitor': stage => first, }
   class { 'profile_base': stage => second, }
   class { 'profile_base::rspec_monitor': stage => second, }
   class { 'profile_rsyslog': stage => second, }
