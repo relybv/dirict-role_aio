4b641bee497184e2eae6f055d5e0114146af6972
Paul Gomersbach <p.gomersbach@rely.nl>
2016-08-03 09:33:08 +0000

    remove stages

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,9 +13,9 @@ class role_aio
   stage{ 'first': }
   stage{ 'second': }
   Stage[first]->Stage[second]
-
-  class { 'profile_nfs': stage => first, }
-  class { 'profile_nfs::rspec_monitor': stage => first, }
+  # nfs was first
+  class { 'profile_nfs': stage => second, }
+  class { 'profile_nfs::rspec_monitor': stage => second, }
   class { 'profile_base': stage => second, }
   class { 'profile_base::rspec_monitor': stage => second, }
   class { 'profile_rsyslog': stage => second, }
